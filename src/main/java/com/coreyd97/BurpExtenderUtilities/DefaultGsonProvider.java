cb2e33082cbb2c5e14db5f404d86794c1a75aa68
Corey-Dean Arthur <corey-dean06@hotmail.com>
2018-11-02 12:28:11 +0000

    Add default gson provider

---
+++ src/main/java/com/coreyd97/BurpExtenderUtilities/DefaultGsonProvider.java
@@ -0,0 +1,29 @@
+package com.coreyd97.BurpExtenderUtilities;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+
+import java.lang.reflect.Type;
+
+public class DefaultGsonProvider implements IGsonProvider {
+
+    private GsonBuilder gsonBuilder;
+    private Gson gson;
+
+    public DefaultGsonProvider(){
+        this.gsonBuilder = new GsonBuilder();
+        this.gson = this.gsonBuilder.create();
+    }
+
+    @Override
+    public Gson getGson() {
+        return this.gson;
+    }
+
+    @Override
+    public void registerTypeAdapter(Type type, TypeAdapter typeAdapter) {
+        this.gsonBuilder.registerTypeAdapter(type, typeAdapter);
+        this.gson = this.gsonBuilder.create();
+    }
+}
