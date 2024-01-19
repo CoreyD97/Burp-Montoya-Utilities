829eda16a89f158b9d27cf4e6c6ba20177cd4ccf
CrazyKidJack <43480837+CrazyKidJack@users.noreply.github.com>
2024-01-19 01:01:44 -0600

    -init NameManager -Make Preferences use NameManager

---
+++ src/main/java/com/coreyd97/BurpExtenderUtilities/NameManager.java
@@ -0,0 +1,21 @@
+package com.coreyd97.BurpExtenderUtilities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class NameManager{
+  public static void reserve(String newName){
+    if(!_nameSet.add(newName))
+      throw new NameCollisionException("Name " + newName + " is already reserved.");
+  }
+
+  public static void release(String name){
+    //this might not actually need to throw an exception... it may not be useful... not sure
+    if(!_nameSet.remove(name))
+      throw new KeyNotReservedException("Name " + name + " was not previously reserved.");
+  }
+
+  public static boolean isReserved(String name){ return _nameSet.contains(name); }
+
+  private static final Set<String> _nameSet = new HashSet<>();
+}
